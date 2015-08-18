9bdd31dbc3959c7543d3297133296e947b6bd550
stepan.sychev@gmail.com <admin3oo3>
2015-08-18 11:48:38 +0600

    CEFLoader fixes

--- CEFLoader.Test/Program.cs
+++ CEFLoader.Test/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Helpers;
 
 namespace CEFLoader.Test
 {
@@ -12,28 +13,47 @@ namespace CEFLoader.Test
         [STAThread]
         static void Main(string[] args)
         {
-            var items =
-                Enumerable.Range(0, 20)
-                    .AsParallel()
-                    .WithDegreeOfParallelism(6)
-                    .Select(i =>
-                    {
-                        var tsk = Task.Factory.StartNew<string>(() =>
+            try
+            { 
+                var items =
+                    Enumerable.Range(0, 5)
+                        .AsParallel()
+                        .WithDegreeOfParallelism(6)
+                        .Select(i =>
                         {
                             var url = "http://google.com/?q=" + i.ToString();
-                            var res = CEFLoader.GetHTML(ref url, new TimeSpan(0,0,10));
-                            return url;
-                        });
-                        tsk.Wait();
-                        return tsk.Result;
-                    }
-                ).ToArray();
+                            try
+                            { 
+                                var tsk = Task.Factory.StartNew<string>(() =>
+                                {
+                                    var res = CEFLoader.GetHTML(ref url, new TimeSpan(0,0,10));
+                                    return url;
+                                });
+                                tsk.Wait();
+                                if (tsk.IsCompleted)
+                                    return tsk.Result;
+                                if (tsk.Exception != null)
+                                    throw tsk.Exception;
+                                return "EMPTY RESULT";
+                            }
+                            catch (Exception ex)
+                            {
+                                Log.Add(ex);
+                                return "ERROR: " + ex.Message;
+                            }
+                        }
+                    ).ToArray();
 
-            foreach(var url in items)
-                Console.WriteLine((string.IsNullOrEmpty(url) ? "[+]" : "[-]") + " NEW URL IS: " + url);
+                foreach(var url in items)
+                    Console.WriteLine((string.IsNullOrEmpty(url) ? "[+]" : "[-]") + " NEW URL IS: " + url);
 
-            Console.ReadKey();
-            CEFLoader.Shutdown(killCurrentProcess : true);
+                Console.ReadKey();
+                CEFLoader.Shutdown(killCurrentProcess : true);
+            }
+            catch(Exception ex)
+            {
+                Log.Add(ex);
+            }
         }
     }
 }
--- CEFLoader/CEFLoader.cs
+++ CEFLoader/CEFLoader.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Helpers;
 
 namespace CEFLoader
 {
@@ -15,39 +16,73 @@ namespace CEFLoader
     {
         static CEFLoader()
         {
-            string resources = Path.Combine(Directory.GetCurrentDirectory(), "cache");
-            if (Directory.Exists(resources))
-                try
-                {
-                    Directory.Delete(resources, true);
-                    Directory.CreateDirectory(resources);
-                }
-                catch { }
-
-            string logFile = Path.Combine(Directory.GetCurrentDirectory(), "chromelog.txt");
-            if (File.Exists(logFile))
-                try
-                {
-                    File.Delete(logFile);
-                }
-                catch
-                {
-                    logFile = Path.Combine(Directory.GetCurrentDirectory(), DateTime.Now.ToString("yyyyMMdd_HHmmss") + "_chromelog.txt");
-                }
-
-            CefSharp.CefSettings settings = new CefSharp.CefSettings()
+            var session = Log.SessionStart("CEFLoader()", true);
+            try
             {
-                CachePath = resources,
-                LogFile = logFile,
-                BrowserSubprocessPath = Path.Combine(Directory.GetCurrentDirectory(), "CefSharp.BrowserSubprocess.exe"),
-                RemoteDebuggingPort = 8088,
-                LogSeverity = LogSeverity.Warning
-            };
-            Cef.OnContextInitialized = delegate
+                Log.Add(session, "Start initialization...");
+                Log.Add(session, string.Format("Current directory for initialization: '{0}'", Helpers.Log.CurrentPath));
+
+                string resources = Path.Combine(Helpers.Log.CurrentPath, "cache");
+                Log.Add(session, string.Format("Cache directory is '{0}'. Directory exists: '{1}'", resources, Directory.Exists(resources)));
+                if (Directory.Exists(resources))
+                    try
+                    {
+                        Directory.Delete(resources, true);
+                        Directory.CreateDirectory(resources);
+                        Log.Add(session, "Cache directory cleared");
+                    }
+                    catch(Exception ex)
+                    {
+                        Log.Add(session, "Can't clear cache directory");
+                        Log.Add(session, ex);
+                    }
+
+                string logFile = Path.Combine(Helpers.Log.CurrentPath, "chromelog.txt");
+                Log.Add(session, string.Format("Log file is '{0}'. Log file exists: '{1}'", logFile, File.Exists(logFile)));
+                if (File.Exists(logFile))
+                    try
+                    {
+                        File.Delete(logFile);
+                    }
+                    catch(Exception ex)
+                    {
+                        Log.Add(session, "Can't remove log file");
+                        Log.Add(session, ex);
+                        logFile = Path.Combine(Helpers.Log.CurrentPath, DateTime.Now.ToString("yyyyMMdd_HHmmss") + "_chromelog.txt");
+                        Log.Add(session, string.Format("New log file is '{0}'. Log file exists: '{1}'", logFile, File.Exists(logFile)));
+                    }
+
+                string subprocessPath = Path.Combine(Helpers.Log.CurrentPath, "CefSharp.BrowserSubprocess.exe");
+                Log.Add(session, string.Format("Subprocess path is '{0}'. Sub process file exists: '{1}'", subprocessPath, File.Exists(subprocessPath)));
+
+                if (!File.Exists(subprocessPath))
+                    throw new Exception("Subprocess file not exists!");
+
+                CefSharp.CefSettings settings = new CefSharp.CefSettings()
+                {
+                    CachePath = resources.Replace(Directory.GetCurrentDirectory() + "\\",""),
+                    LogFile = logFile.Replace(Directory.GetCurrentDirectory() + "\\", ""),
+                    BrowserSubprocessPath = subprocessPath.Replace(Directory.GetCurrentDirectory() + "\\", ""),
+                    RemoteDebuggingPort = 8088,
+                    LogSeverity = Debugger.IsAttached ? LogSeverity.Verbose : LogSeverity.Warning
+                };
+                Cef.OnContextInitialized = delegate
+                {
+                    Cef.GetGlobalCookieManager().SetStoragePath("cookies", true);
+                };
+                Log.Add(session, "Start Cef.Initialize()");
+                Cef.Initialize(settings, shutdownOnProcessExit: true, performDependencyCheck: !Debugger.IsAttached);
+                Log.Add(session, "Cef.Initialize() done");
+            }
+            catch(Exception ex)
             {
-                Cef.GetGlobalCookieManager().SetStoragePath("cookies", true);
-            };
-            Cef.Initialize(settings, shutdownOnProcessExit: true, performDependencyCheck: !Debugger.IsAttached);
+                Log.Add(session, ex);
+                throw;
+            }
+            finally
+            {
+                Log.SessionEnd(session);
+            }
         }
 
         public static void Shutdown(bool killCurrentProcess = false)
